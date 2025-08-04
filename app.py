523ab4ace935349501537e0a8334cc017729edcb
jake05050505 <jakehancox12@gmail.com>
2025-08-04 21:47:54 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-import argparse
-import sys
-import time
+from argparse import ArgumentParser
+from pathlib import Path
+from sys import argv
+from time import sleep
 import winsound
 
 def main():
+    # region inputs
     # if the program was run via running metronome.py from explorer or bpm value is missing
-    if len(sys.argv) == 1:
+    if len(argv) == 1:
         value = int(input("Please enter BPM:"))
         delay_sec = 60 / value
         bpm = 60 / delay_sec
     else:
-        parser = argparse.ArgumentParser(description="Simple metronome")
+        parser = ArgumentParser(description="Simple metronome")
         parser.add_argument(
             "value",
             type=float,
@@ -24,15 +26,18 @@ def main():
 
         delay_sec = args.value / 1000.0 if args.delay else 60.0 / args.value
         bpm = 60.0 / delay_sec
+    # endregion
 
-    print(f"Metronome: {bpm:.2f} BPM ({delay_sec*1000:.1f} ms interval). Ctrl+C to stop.")
+    print(f"Metronome: {bpm:.2f} BPM ({delay_sec*1000:.1f} ms interval).")
 
     try:
         while True:
-            winsound.PlaySound(f"{__file__}/../sounds/stick.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-            time.sleep(delay_sec)
+            winsound.PlaySound(f"{Path(__file__).parent}/sounds/stick.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+            sleep(delay_sec)
     except KeyboardInterrupt:
         print("\nStopped.")
+
+    return 0
 
 if __name__ == "__main__":
     main()
